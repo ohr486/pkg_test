d3346670cbee0e798ef596abeb148dbbfdab6803
ohr486 <ohr486@gmail.com>
2018-09-26 16:33:27 +0900

    fix mix

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule PgkTest.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      description: "description for pkg_test",
+      package: package(),
       deps: deps()
     ]
   end
@@ -21,8 +23,15 @@ defmodule PgkTest.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp package() do
+    [
+      maintainers: ["ohr486"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/elixir-ecto/postgrex"}
     ]
   end
 end
